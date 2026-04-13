f29f7ad0db99bbbb48b60ef7684a556af98139e4
Matthew Nowakowski <nowakowskim98@gmail.com>
2026-04-13 15:33:55 -0400

    Basic echo screen

--- phrazzle/lib/main.dart
+++ phrazzle/lib/main.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:phrazzle/entered_texts.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class _MainAppState extends State<MainApp> {
+  final List<String> texts = ['test'];
+  String textToAdd = '';
+
+  void addText(String text) {
+    setState(() {
+      texts.add(text);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                onChanged: (final text) {
+                  setState(() {
+                    textToAdd = text;
+                  });
+                },
+              ),
+              TextButton(
+                onPressed: () => addText(textToAdd),
+                child: Text('Add'),
+              ),
+              Expanded(child: EnteredTexts(texts)),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MainAppState();
 }
