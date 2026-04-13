1c1938c6652d03b2101f82d23439197da66531a0
Matthew Nowakowski <nowakowskim98@gmail.com>
2026-04-13 17:12:43 -0400

    Partially implement round loop

--- phrazzle/lib/game.dart
+++ phrazzle/lib/game.dart
@@ -18,31 +18,98 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
-  final players = <_Player>[];
   String inputValue = '';
-  String prompt = 'Enter player name:';
   _FormState formState = .players;
+
+  final players = <_Player>[];
+  String rootPhrase = '';
+  int playerEntriesIndex = 0;
+  final entries = <String>[];
+
+  String prompt() {
+    switch (formState) {
+      case .players:
+        return 'Enter player name:';
+      case .rootPhrase:
+        return 'Enter starting phrase';
+      case .entries:
+        return 'Enter entries:';
+      case .winners:
+        return 'Winners:';
+    }
+  }
 
   void commitText(String text) {
     if (text.isEmpty) return;
 
+    setState(() {
+      switch (formState) {
+        case .players:
+          players.add(_Player(text, text, 0));
+          break;
+        case .rootPhrase:
+          rootPhrase = inputValue;
+          break;
+        case .entries:
+          entries.add(text);
+          break;
+        case .winners:
+          break;
+      }
+    });
+  }
+
+  void end() {
+    setState(() {
+      switch (formState) {
+        case .players:
+          rootPhrase = '';
+          formState = .rootPhrase;
+          break;
+        case .rootPhrase:
+          entries.clear();
+          formState = .entries;
+          break;
+        case .entries:
+          players[playerEntriesIndex].score = 0; // TODO: Get score from lib
+          if (++playerEntriesIndex >= players.length) {
+            formState = .winners;
+          }
+          entries.clear();
+          break;
+        case .winners:
+          players.clear();
+          formState = .players;
+          break;
+      }
+    });
+  }
+
+  ListView? getList() {
     switch (formState) {
       case .players:
-        setState(() => players.add(_Player(text, text, 0)));
-        break;
+        return ListView(
+          children: [
+            for (final player in players)
+              Text('${player.id} - ${player.name} - ${player.score}'),
+          ],
+        );
       case .rootPhrase:
-        break;
+        return null;
       case .entries:
-        break;
+        return ListView(children: [for (final entry in entries) Text(entry)]);
       case .winners:
-        break;
+        return null;
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: .start,
       children: [
+        Text(rootPhrase),
+        Text(prompt()),
         Row(
           children: [
             Expanded(
@@ -58,15 +125,8 @@ class _GameState extends State<Game> {
             ),
           ],
         ),
-        Text(prompt),
-        Expanded(
-          child: ListView(
-            children: [
-              for (final player in players)
-                Text('${player.id} - ${player.name} - ${player.score}'),
-            ],
-          ),
-        ),
+        Expanded(child: getList() ?? Text('No data')),
+        TextButton(onPressed: end, child: Text('End')),
       ],
     );
   }
