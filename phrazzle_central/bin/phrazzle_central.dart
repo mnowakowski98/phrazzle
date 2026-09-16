c2d3fdbed0c3acddfe794c0c1159862c0ba2bdb5
Matthew Nowakowski <nowakowskim98@gmail.com>
2026-09-16 15:00:32 -0400

    Added headers to accept cross origin requests from anywhere

--- phrazzle_central/bin/phrazzle_central.dart
+++ phrazzle_central/bin/phrazzle_central.dart
@@ -1,6 +1,7 @@
 import 'package:phrazzle_central/phrazzle_central.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 final app = Router();
@@ -13,5 +14,9 @@ void main(List<String> arguments) async {
       //   }),
       // )
       .add(PhrazzleCentral().router.call);
-  await serve(cascade.handler, 'localhost', 3000);
+
+  final pipeline = Pipeline()
+      .addMiddleware(corsHeaders())
+      .addHandler(cascade.handler);
+  await serve(pipeline, 'localhost', 3000);
 }
