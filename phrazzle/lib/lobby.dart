384e172e811918d159eeac2e18d5d36ad9a35457
Matthew Nowakowski <nowakowskim98@gmail.com>
2026-09-20 20:06:09 -0400

    Made starting phrase enterable

--- phrazzle/lib/lobby.dart
+++ phrazzle/lib/lobby.dart
@@ -5,25 +5,56 @@ import 'package:http/http.dart' as http;
 import 'package:phrazzle/player_list.dart';
 import 'package:phrazzle_lib/phrazzle.dart';
 
-class Lobby extends StatelessWidget {
+class Lobby extends StatefulWidget {
   final Phrazzle game;
 
   const Lobby(this.game, {super.key});
 
   @override
+  State<Lobby> createState() => _LobbyState();
+}
+
+class _LobbyState extends State<Lobby> {
+  final startingPhraseController = TextEditingController();
+  var allowPhraseEntry = false;
+
+  @override
+  void initState() {
+    super.initState();
+    startingPhraseController.addListener(() {
+      setState(
+        () => allowPhraseEntry = startingPhraseController.text.isNotEmpty,
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        PlayerList(game.players.values.toList()),
-        TextButton(
-          onPressed: () async {
-            // TODO: Remove hardcoded url
-            // TODO: Make start phrase user enterable
-            await http.put(
-              Uri.parse('http://localhost:3000/game/testingstartphrase'),
-            );
-          },
-          child: Text('Start'),
+        PlayerList(widget.game.players.values.toList()),
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: startingPhraseController,
+                decoration: InputDecoration(hintText: 'Starting phrase'),
+              ),
+            ),
+            TextButton(
+              onPressed: allowPhraseEntry
+                  ? () async {
+                      // TODO: Remove hardcoded url
+                      await http.put(
+                        Uri.parse(
+                          'http://localhost:3000/game/${startingPhraseController.text}',
+                        ),
+                      );
+                    }
+                  : null,
+              child: Text('Start'),
+            ),
+          ],
         ),
       ],
     );
